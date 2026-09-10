734799fb82de049dfeb4436cd961b12ecf946191
Filip Milosevic <filipmcetinje@gmail.com>
2026-09-10 13:17:16 -0400

    Add artist section to home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,31 @@
 import Hero from "../../components/Hero/Hero";
+import selfPortrait from "../../assets/artwork/self-portrait.jpg";
+import "./Home.css";
 
 function Home() {
-  return <Hero />;
+  return (
+    <main>
+      <Hero />
+
+      <section className="home__about">
+        <img
+          className="home__about-image"
+          src={selfPortrait}
+          alt="Self-portrait artwork by Filip Milosevic"
+        />
+
+        <div className="home__about-content">
+          <h2 className="home__about-title">About the Artist</h2>
+
+          <p className="home__about-text">
+            I’m Filip Milosevic, an artist and software developer. I created
+            Luma to connect travel in Montenegro with photography, painting, and
+            creative inspiration.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Home;
