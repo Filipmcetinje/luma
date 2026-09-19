82b0d6620b44d256082973f34ff3d5ad8262e20d
Filip Milosevic <filipmcetinje@gmail.com>
2026-09-18 21:15:57 -0400

    Simplify discover categories

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
 import "./Hero.css";
+import heroImage from "../../assets/hero/montenegro-hero.jpg";
 
 function Hero() {
   return (
-    <section className="hero">
+    <section
+      className="hero"
+      style={{
+        backgroundImage: `linear-gradient(
+      rgba(0, 0, 0, 0.45),
+      rgba(0, 0, 0, 0.45)
+    ), url(${heroImage})`,
+      }}
+    >
       <h1 className="hero__title">Discover the world through creativity.</h1>
 
       <p className="hero__text">
@@ -10,7 +20,9 @@ function Hero() {
         creative moment.
       </p>
 
-      <button className="hero__button">Start Exploring</button>
+      <Link className="hero__button" to="/discover">
+        Start Exploring
+      </Link>
     </section>
   );
 }
